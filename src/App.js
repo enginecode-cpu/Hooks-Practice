c2811355280e0d027c7d85a4ad4be51805259ce2
enginecode-cpu <bassmatics1002@gmail.com>
2020-12-12 20:11:41 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useInput from "./libs/useInput";
+import useTabs from "./libs/useTabs";
+import "./App.css";
 
 function App() {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+
+  const content = [
+    {
+      tab: "Section 1",
+      content: "This is Section1",
+    },
+    {
+      tab: "Section 2",
+      content: "This is Section2",
+    },
+  ];
+
+  const { currentItem, changeItem } = useTabs(0, content);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="useInput">
+        <h1>Hello</h1>
+        <input value={name.value} onChange={name.onChange} />
+      </div>
+      <div className="useTabs">
+        {content.map((section, index) => (
+          <button key={section.tab} onClick={() => changeItem(index)}>
+            {section.tab}
+          </button>
+        ))}
+        <div>{currentItem.content}</div>
+      </div>
     </div>
   );
 }
